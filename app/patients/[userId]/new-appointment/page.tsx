9369cc6daf77ac6ede63e3e0bc965eef74e5a78f
Adeleye Ifaturoti <adeleyeifaturoti@yahoo.com>
2024-09-22 19:36:53 +0100

    created AppointmentForm.tsx

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,5 +1,32 @@
-const Page = () => {
-  return <div>New Appointment</div>;
-};
+import Copyright from "@/components/Copyright";
+import PatientForm from "@/components/forms/PatientForm";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
-export default Page;
+export default function NewAppointment() {
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w[860px] flex-1 justify-between">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            width={1000}
+            height={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+          {/* <AppointmentForm /> */}
+          <Copyright />
+        </div>
+      </section>
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1000}
+        width={1000}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+      />
+    </div>
+  );
+}
---
+++ components/Copyright.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Copyright = () => {
+  return (
+    <p className="justify-items-end text-dark-600 xl:text-left">
+      Copyright © {new Date().getFullYear()} CarePulse
+    </p>
+  );
+};
+
+export default Copyright;
